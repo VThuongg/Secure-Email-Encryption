3502f8396ba337e8d7a8f3ed4efe0f8677598890
Thuong <thuong16052004@gmail.com>
2026-06-04 10:02:22 +0700

    Fix password length for PostgreSQL and setup dotenv

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     public_key = db.Column(db.String, nullable=False)
     private_key = db.Column(db.String, nullable=False)
     backup_email = db.Column(db.String(120), nullable=True)
